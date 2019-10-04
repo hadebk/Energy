58d5b3182d4596a892b9b1b06453d70372747a4c
abdalhadi bakr <hade.bk.1998@gmail.com>
2019-10-04 18:40:40 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-$(function () {
-    $(window).on("scroll", function () { /* change the color of nav when scroll down */
-        var sc = $(window).scrollTop();
-        if (sc > 100) {
-            // scroll
-            $("nav").css("background-color", "#ffcc00");
-            $("#navbar-box #nav-menu ul li a").css("color", "#04054f");
-            $("#navbar-box #nav-hamburger i").css("color", "#04054f");
-        }
-    });
+$(document).ready(function() {
+  /* change the color of nav when scroll down */
+  $(window).on("scroll", function() {
 
+    var sc = $(window).scrollTop();
+    if (sc > 100) {
+      // scroll
+      $("nav").css("background-color", "#ffcc00");
+      $("#navbar-box #nav-menu ul li a").css("color", "#04054f");
+      $("#navbar-box #nav-hamburger i").css("color", "#04054f");
+    }
+  });
 
-});
+})
