7abe93f75d0d33c497c187391cefb915b50898bc
abdalhadi bakr <hade.bk.1998@gmail.com>
2019-10-07 23:55:59 +0200

    init

--- js/main.js
+++ js/main.js
@@ -5,10 +5,17 @@ $(document).ready(function() {
     var sc = $(window).scrollTop();
     if (sc > 100) {
       // scroll
-      $("nav").css("background-color", "#ffcc00");
+      /*$("nav").css("background-color", "#ffcc00");
       $("#navbar-box #nav-menu ul li a").css("color", "#04054f");
-      $("#navbar-box #nav-hamburger i").css("color", "#04054f");
+      $("#navbar-box #nav-hamburger i").css("color", "#04054f");*/
     }
   });
+
+  // flip icon of the service box when hover on it
+  $("#services-box .services").hover(function(){
+    $(this).find('img').addClass("animated flipInY")
+  },function(){
+    $(this).find('img').removeClass("animated flipInY")
+  })
 
 })
